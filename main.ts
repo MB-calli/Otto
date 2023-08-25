370e591f5d4fcb8eac7939f26f88d32a12a77eb5
MB-calli <143164730+MB-calli@users.noreply.github.com>
2023-08-25 09:51:54 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,65 @@
-function vorwärtz () {
+function vorwärts () {
     pins.servoWritePin(AnalogPin.C5, 60)
-    control.waitMicros(100000)
+    control.waitMicros(100)
     pins.servoWritePin(AnalogPin.P2, 110)
     pins.servoWritePin(AnalogPin.P1, 110)
+    control.waitMicros(200)
+    pins.servoWritePin(AnalogPin.C5, 90)
+    control.waitMicros(100)
+    pins.servoWritePin(AnalogPin.C6, 120)
+    control.waitMicros(100)
+    pins.servoWritePin(AnalogPin.P1, 60)
+    pins.servoWritePin(AnalogPin.P2, 60)
+    control.waitMicros(200)
+    pins.servoWritePin(AnalogPin.C6, 90)
+    control.waitMicros(100)
 }
+function hip () {
+    pins.servoWritePin(AnalogPin.P1, 90)
+    control.waitMicros(100)
+    pins.servoWritePin(AnalogPin.P2, 90)
+    control.waitMicros(100)
+    pins.servoWritePin(AnalogPin.C5, 60)
+    pins.servoWritePin(AnalogPin.C6, 120)
+    control.waitMicros(400)
+    pins.servoWritePin(AnalogPin.C5, 90)
+    pins.servoWritePin(AnalogPin.C6, 90)
+    control.waitMicros(400)
+}
+function rückwärts () {
+    pins.servoWritePin(AnalogPin.C5, 60)
+    control.waitMicros(100)
+    pins.servoWritePin(AnalogPin.P2, 60)
+    pins.servoWritePin(AnalogPin.P1, 60)
+    control.waitMicros(200)
+    pins.servoWritePin(AnalogPin.C5, 90)
+    control.waitMicros(100)
+    pins.servoWritePin(AnalogPin.C6, 120)
+    control.waitMicros(100)
+    pins.servoWritePin(AnalogPin.P1, 120)
+    pins.servoWritePin(AnalogPin.P2, 120)
+    control.waitMicros(200)
+    pins.servoWritePin(AnalogPin.C6, 90)
+    control.waitMicros(100)
+}
+function stehen () {
+    pins.servoWritePin(AnalogPin.P1, 90)
+    control.waitMicros(100)
+    pins.servoWritePin(AnalogPin.P2, 90)
+    control.waitMicros(100)
+    pins.servoWritePin(AnalogPin.C5, 90)
+    control.waitMicros(100)
+    pins.servoWritePin(AnalogPin.C6, 90)
+    control.waitMicros(100)
+}
+basic.clearScreen()
+stehen()
 basic.forever(function () {
-	
+    if (pins.analogReadPin(AnalogPin.C16) > 600) {
+        vorwärts()
+    } else if (pins.analogReadPin(AnalogPin.C16) < 400) {
+        rückwärts()
+    } else if (input.buttonIsPressed(Button.A)) {
+        hip()
+    }
 })
